03ab55807fa9f7b4e2bc80e8557c866832d94d84
chayot <chayot>
2021-12-31 17:03:43 +0100

    petites optimisations

---
+++ js/animations.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', animateElements)
+window.addEventListener('scroll', animateElements)
+
+function animateElements () {
+    let elts = document.querySelectorAll('[data-animation-class]')
+    for (let elt of elts) {
+
+        if (window.scrollY + window.innerHeight > elt.offsetTop) console.log(elt), handleElementAnimation(elt)
+
+    }
+}
+
+function handleElementAnimation (elt) {
+
+    if (elt.dataset.triggered) return
+
+    const animation_duration = elt.dataset.animationDuration || 700
+    const animation_class = elt.dataset.animationClass
+    elt.dataset.trigerred = true
+
+    elt.classList.add(animation_class)
+
+    window.setTimeout(function () {
+        delete elt.dataset.animationClass
+        delete elt.dataset.animationDuration
+        delete elt.dataset.trigerred
+    }, animation_duration)
+
+}
